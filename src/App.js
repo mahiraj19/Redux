742517a814a4b0c29ea79adcd23a31ef7033979c
mahiraj2000 <mahirajdeora7773@gmail.com>
2023-12-18 15:23:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { ClearNumber, DecrementNumber, IncrementNumber } from './actions';
+import UserList from './USERS/Users';
+function App() {
+  const dispatch = useDispatch()
+  const myState = useSelector((state) => state.ChangeTheNumber )
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+      {/* <UserList/> */}
+      <h3>Count:{myState}</h3>
+      <button onClick={() => dispatch(IncrementNumber())}>Increment(+)</button>
+      <button onClick={() => dispatch(DecrementNumber())}>Decrement(-)</button>
+      <button onClick={() => dispatch(ClearNumber())}>ClearCount</button>
+
+    </div>
+  );
+}
+
+export default App;
